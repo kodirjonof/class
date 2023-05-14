6632f2ec558325c0bf4ab90e4ce9e14df0464073
kodirjonof <72494208+kodirjonof@users.noreply.github.com>
2023-05-14 23:54:15 +0500

    first commit

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Slider from './slider'
+
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Slider/>
+    </div>
+  )
+}
+
+export default Home
